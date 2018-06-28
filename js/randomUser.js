441fd88c96bd6b41998f6f3a5c05402e767700b8
Cody Reed <cody2j2reed@gmail.com>
2018-06-28 15:31:50 -0400

    completed loop of employee modal index

--- js/randomUser.js
+++ js/randomUser.js
@@ -1,82 +1,92 @@
+
+
 // Create ajax request
-$('document').ready(function () {
+  let dataResults = [];
+
   $.ajax({
     url: 'https://randomuser.me/api/?results=12&nat=us',
     dataType: 'json',
     //if successful create employee directory
     success: function(data) {
+
+      dataResults = data.results;
       var employeeHTML = '';
-      $.each(data.results, function(i, employee) {
+      $.each(dataResults, function(index, employee) {
         employeeHTML += '<div class="employee">';
         employeeHTML += '<div class="image">';
         employeeHTML += '<img src="' + employee.picture.medium + '"></div>';
         employeeHTML += '<div class="info">';
         employeeHTML += '<p class="employeeName">' + employee.name.first + ' ' + employee.name.last +'</p>';
-        employeeHTML += '<a class="employeeEmail" href="' + employee.email + '"> ' +employee.email +' </a>';
-        employeeHTML += '<p class=employeeState>' + employee.location.city +'</p></li></div></div>';
+        employeeHTML += '<a class="employeeEmail" href=""> ' +employee.email +' </a>';
+        employeeHTML += '<p class="employeeState">' + employee.location.city +'</p></div></div>';
       });
-      $('#directory').html(employeeHTML);//attach pullRequest to html
-    }
-  });// select all buttons
-});// end ready
+      $('#directory').html(employeeHTML);//attach pullRequest to directory
 
-// Overlay Modal
-// var modal = (function(){
-//   var $window = $(window);
-//   var $modal = $('<div class="modal"/>');
-//   var $content = $('<div class="modal-content"/>');
-//   var $close = $('<button role="button" class="modal-close">close</button>');
-//
-//   //Append close button to modal window
-//   $modal.append($content, $close);
-//
-//   //Close on click
-//   $close.on('click', function(e) {
-//     e.preventDefault();
-//     modal.close();
-//   });
-//
-//   return {
-//     //Add code to center modal
-//     //Define center
-//     center: funtion() {
-//       var top = Math.max($window.height() - $modal.outerHeight(), 0) / 2;
-//       var left = Math.max($window.width() - $modal.outerWidth(), 0) / 2;
-//       //Set modal to center in CSS
-//       $modal.css({
-//         top: top + $window.scrollTop(),
-//         left: left + $window.scrollLeft()
-//       });
-//     },
-//     //Define open or show of modal object
-//     open: function(settings) {
-//       //Set new content of modal
-//       $content.empty().append(settings.content);
-//
-//       //Set modal dimensions
-//       $modal.css({
-//         width: settings.width || 'auto',
-//         height: settings.height || 'auto'
-//         //Add to page
-//       }).appendTo('body');
-//
-//       //Call center() method
-//       $modal.center();
-//       $(window).on('resize', modal.center);
-//     },
-//     //Define close() method
-//     close: function() {
-//       $content.empty();
-//       $modal.detach();
-//       $(window).off('resize', modal.center);
-//     }
-//   };
-// }());
-//
-// //Remove modal from page
-// var $content = $('#modal').detach();
-//
-// //Click event on employee
-// $('.employee').on('click', function(e) {
-//   modal.open({});
-// });
+      // Define click event on employee directory to create modal
+      profiles = $('#directory .employee');
+
+      // for each profile in the employee directory
+      for (let i = 0; i < profiles.length; i++) {
+
+        // When a profile is clicked
+        profiles[i].onclick = function () {
+
+          // Diplay modal and call modalFill
+          $('#modal').show();
+          modalFill(dataResults[i], i);
+        };
+
+        // When modal close button is clicked
+        $('#button').on('click', function () {
+
+          // Hide modal
+          $('#modal').hide();
+        });
+      }
+
+      // Define modalFill function
+      function modalFill(target, index) {
+        // Define content variable for modal fill
+        let employeeModal = '';
+        currentTarget = dataResults.indexOf(target);
+        // Store new Date string from  value of (date:key) in dataResults
+        let dob = new Date(target.dob.date);
+        dob = dob.toLocaleDateString();
+
+        // Add content to employeeModal
+        employeeModal += '<img class="imgLarge" src="' + target.picture.large +'">';
+        employeeModal += '<p class="employeeName">' + target.name.first + ' ' + target.name.last +'</p>';
+        employeeModal += '<a class="employeeEmail" href=""> ' + target.email +' </a>';
+        employeeModal += '<p class="employeeState">' + target.location.city +'</p>';
+        employeeModal += '<hr>';
+        employeeModal += '<p class="employeePhone">' + target.phone + '</p>';
+        employeeModal += '<p class="employeeAddress">' + target.location.street + ',' + ' ' + '<span class="state">' + target.location.state + '</span>' + ' ' + target.location.postcode + '</p>';
+        employeeModal += '<p class="employeeDOB">Birthday:' + ' ' + dob +'</p>';
+
+        $('#modal-profile').html(employeeModal);//attach pullRequest to modal-profile
+
+          // When right modal button is clicked
+         $("#rightBtn").click(function() {
+
+                    // If employee is eleventh index of dataResults
+                    if(index === 11){
+                      // Fill modal profile with employee content @ index 0
+                      modalFill(dataResults[0], 0)
+                    }// Else fill modal with next index employee content
+                     else {
+                     modalFill(dataResults[index + 1], index + 1);
+                   }
+                 });
+
+          // Same as above, just reversed
+         $("#leftBtn").click(function() {
+           if(index === 0){
+             modalFill(dataResults[11], 11)}
+             else {
+              modalFill(dataResults[index - 1], index - 1);
+            }
+         });
+
+      } // end of modalFill function
+    } // end of success
+  }); // end of ajax
